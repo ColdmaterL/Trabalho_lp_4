1d2b42bf4086b37c50f3d2a029e7e2fa176ca8f4
ColdmaterL <67527025+ColdmaterL@users.noreply.github.com>
2021-05-10 10:16:22 -0300

    Update Contato.hs

--- Contato.hs
+++ Contato.hs
@@ -2,15 +2,17 @@ data Contato = Contato{name :: String, tel :: Int, ende :: String, relac :: Stri
 
 insereContato :: Contato -> Contato
 insereContato c = [c]
-insereContato (n1 : n2) c
-    |(name n1 == name c) = c : n2
-    |otherwise = n1 : insereContato c
+insereContato (cab : cal) c
+    |(name cab == name c) = c : cal
+    |(null cal) = c
+    |otherwise = cab : insereContato c
     
 removeContato :: Contato -> String -> Contato
 removeContato [] c = Contato "" 00000000 "" ""
-removeContato (n1 : n2) c
-    |(nome n1 == nome c) = n2
-    |otherwise = removeContato n2 c
+removeContato (cab : cal) c
+    |(nome cab == nome c) = cal
+    |(null cal) = c
+    |otherwise = cab : removeContato cal c
 
 printaContato :: Contato -> String
 printaContato c = name c ++ show(tel c) ++ ende c ++ relac c ++ "\n" ++ printaContato(tail c)
